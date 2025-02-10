82910d1a00e7fa6cf3e7fcb8c642f5fba92f2ae2
Vidyun Agarwal <vidyunagarwal@gmail.com>
2025-02-11 01:10:58 +0530

    project deployment

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://accurate-tick-43.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/constants/templates.ts
@@ -0,0 +1,121 @@
+export const templates = [
+    { id: "blank", 
+      label: "Blank Document", 
+      imageUrl: "/blank-document.svg",
+      initialContent: "",
+    },
+    { id: "software-proposal", 
+      label: "Software Development Proposal", 
+      imageUrl: "/software-proposal.svg",
+      initialContent: `
+        <h1>Software Proposal</h1>
+        <p>Project Overview & Key Details</p>
+        
+        <h2>Introduction</h2>
+        <p>A brief outline of the proposed software solution.</p>
+
+      <h2>Scope & Features</h2>
+        <p>Key functionalities and expected outcomes.</p>
+
+        <h2>Timeline & Pricing</h2>
+        <p>Estimated duration and cost breakdown.</p>
+
+        <p>Contact: email@example.com</p>
+      `
+    },
+    { id: "project-proposal", 
+        label: "Project Proposal", 
+        imageUrl: "/project-proposal.svg",
+        initialContent: `
+        <h1>Project Proposal</h1>
+        <p>Project Overview & Key Details</p>
+        
+        <h2>Introduction</h2>
+        <p>A brief outline of the proposed software solution.</p>
+
+      <h2>Scope & Features</h2>
+        <p>Key functionalities and expected outcomes.</p>
+
+        <h2>Timeline & Pricing</h2>
+        <p>Estimated duration and cost breakdown.</p>
+
+        <p>Contact: email@example.com</p>
+      ` 
+    },
+    { id: "business-letter", 
+        label: "Business Letter", 
+        imageUrl: "/business-letter.svg",
+        initialContent: `
+        <h1>Business Letter</h1>
+        <p>Project Overview & Key Details</p>
+        
+        <h2>Introduction</h2>
+        <p>A brief outline of the proposed software solution.</p>
+
+      <h2>Scope & Features</h2>
+        <p>Key functionalities and expected outcomes.</p>
+
+        <h2>Timeline & Pricing</h2>
+        <p>Estimated duration and cost breakdown.</p>
+
+        <p>Contact: email@example.com</p>
+      `
+    },
+    { id: "resume", 
+        label: "Resume", 
+        imageUrl: "/resume.svg",
+        initialContent: `
+        <h1>Resume</h1>
+        <p>Project Overview & Key Details</p>
+        
+        <h2>Introduction</h2>
+        <p>A brief outline of the proposed software solution.</p>
+
+      <h2>Scope & Features</h2>
+        <p>Key functionalities and expected outcomes.</p>
+
+        <h2>Timeline & Pricing</h2>
+        <p>Estimated duration and cost breakdown.</p>
+
+        <p>Contact: email@example.com</p>
+      `
+    },
+    { id: "cover-letter", 
+      label: "Cover Letter", 
+      imageUrl: "/cover-letter.svg",
+      initialContent: `
+        <h1>Cover Letter</h1>
+        <p>Project Overview & Key Details</p>
+        
+        <h2>Introduction</h2>
+        <p>A brief outline of the proposed software solution.</p>
+
+      <h2>Scope & Features</h2>
+        <p>Key functionalities and expected outcomes.</p>
+
+        <h2>Timeline & Pricing</h2>
+        <p>Estimated duration and cost breakdown.</p>
+
+        <p>Contact: email@example.com</p>
+      `
+    },
+    { id: "letter", 
+      label: "Letter", 
+      imageUrl: "/letter.svg",
+      initialContent: `
+        <h1>Letter</h1>
+        <p>Project Overview & Key Details</p>
+        
+        <h2>Introduction</h2>
+        <p>A brief outline of the proposed software solution.</p>
+
+      <h2>Scope & Features</h2>
+        <p>Key functionalities and expected outcomes.</p>
+
+        <h2>Timeline & Pricing</h2>
+        <p>Estimated duration and cost breakdown.</p>
+
+        <p>Contact: email@example.com</p>
+      `
+    } 
+];
